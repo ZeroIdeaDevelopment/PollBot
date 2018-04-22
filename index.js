9d79b04ee88e07463983177a758b9e251608d258
Lewis Crichton <lewistehminerz@outlook.com>
2018-04-22 20:37:47 +0100

    add some events

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ const options = require('./config.json');
 const bot = new eris(options.token, {
     shards: options.shards
 });
+bot.db = new redite(options.redis);
+
+bot.on('messageCreate', msg => {
+    
+});
+
+bot.on('messageReactionAdd', msg => {
+    
+});
